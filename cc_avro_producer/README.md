9def8fef7d1c03dd09e864d935732929af126971
Brijesh Jaggi <brijesh.jaggi@gmail.com>
2024-02-09 16:54:23 -0500

    latest code

---
+++ cc_avro_producer/README.md
@@ -0,0 +1,5 @@
+
+
+## Observations
+
+During our tests we found that the request count dropped dramatically when batch size was increased to 50K and linger.ms was used to 50ms.
---
+++ config.py
@@ -0,0 +1,25 @@
+confluent_cluster_config = {
+    'bootstrap.servers': 'pkc-ep9mm.us-east-2.aws.confluent.cloud:9092',
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanisms': 'PLAIN',
+    'sasl.username': 'CUELITMMWZFYAJM3',
+    'sasl.password': 'kZN1W+mFZdNp4qytzgU3qWxBbmJ1YLMZviONTNMTK0Qvt8vIhkp3X7Krzqgk3Gts',
+    'client.id': 'jaggi_laptop_client',
+    'delivery.timeout.ms' : '5000',
+    'request.timeout.ms' : '5000',
+    'batch.size' : '55000',
+    'linger.ms' : '50'
+
+}
+
+confluent_sr_config = {
+    'url': 'https://psrc-j55zm.us-central1.gcp.confluent.cloud',
+    'basic.auth.user.info': 'W4ZYQ2242AWXRNFJ:INX4avuDPSlb9QstqdycWQqsF54DR/H8JQakYytCKgRORSgYCyrG/HhJ5CIzCgDY'
+}
+
+client_config = {
+    'topic_name' : 'test',
+    'consumer_group' : 'test_cg'
+}
+
+
